0994207ae3154dcf1ce0a966a2399e3c2e13b925
Domis201 <margisdominykas@gmail.com>
2024-05-04 20:24:56 +0300

    initial

---
+++ Assets/Enemy.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Chicken bird = collision.collider.GetComponent<Chicken>();
+        if (bird != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        Enemy enemy = collision.collider.GetComponent<Enemy>();
+        if (enemy != null)
+        {
+            return;
+        }
+
+        if (collision.contacts[0].normal.y < -0.5)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
