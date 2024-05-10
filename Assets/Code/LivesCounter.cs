959ea6f44ecabbf767248c9573099dc623cc83fd
Osvaldas Žilinskas <120989501+0svis@users.noreply.github.com>
2024-05-11 02:31:24 +0300

    Level 4
    Level 4 scene
    Bug fixes at level 1 scene

--- Assets/Code/LivesCounter.cs
+++ Assets/Code/LivesCounter.cs
@@ -29,7 +29,7 @@ public class Lives : MonoBehaviour
         if (currentLives == 0) 
         {
             return;
-        }
+		}
         else
         {
             currentLives = currentLives - amount;
